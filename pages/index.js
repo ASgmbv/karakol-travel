f0071222d567d6beffc534d54f58c3dfedace3bb
Alymbek Sagymbaev <38725027+ASgmbv@users.noreply.github.com>
2020-09-20 02:56:21 +0600

    fddfd

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home({ tours }) {
                     "url":
                       "https://trello-attachments.s3.amazonaws.com/5f5623dda5f75f7368d0df19/5f564e36fe929c34c960c32a/30a8e4c90533f9e9a7d45fcb94de2b12/photo5354937067303448055-min.jpg",
                     "height": "406",
-                    "width": "305",
+                    "width": "305"
                   },
                   "step": [
                     {
@@ -63,15 +63,15 @@ export default function Home({ tours }) {
                       "name": "Перейдите на сайт",
                       "itemListElement": {
                         "@type": "HowToDirection",
-                        "text": "Выберите тур, который вам порнавился.",
+                        "text": "Выберите тур, который вам порнавился."
                       },
                       "image": {
                         "@type": "ImageObject",
                         "url":
                           "https://trello-attachments.s3.amazonaws.com/5f5623dda5f75f7368d0df19/5f564e36fe929c34c960c32a/30a8e4c90533f9e9a7d45fcb94de2b12/photo5354937067303448055-min.jpg",
                         "height": "406",
-                        "width": "305",
-                      },
+                        "width": "305"
+                      }
                     },
                     {
                       "@type": "HowToStep",
@@ -79,15 +79,15 @@ export default function Home({ tours }) {
                       "url": "https://karakol-travel.vercel.app/",
                       "itemListElement": {
                         "@type": "HowToTip",
-                        "text": "Выберите тур из списка, предоставленных.",
+                        "text": "Выберите тур из списка, предоставленных."
                       },
                       "image": {
                         "@type": "ImageObject",
                         "url":
                           "https://trello-attachments.s3.amazonaws.com/5f5623dda5f75f7368d0df19/5f56259008b4584a42f7fb24/fff52be0e8c64935324fb7e797276561/photo5352685267489763105-min.jpg",
                         "height": "406",
-                        "width": "305",
-                      },
+                        "width": "305"
+                      }
                     },
                     {
                       "@type": "HowToStep",
@@ -96,16 +96,16 @@ export default function Home({ tours }) {
                       "itemListElement": {
                         "@type": "HowToTip",
                         "text":
-                          "Позвоните по номеру на сайте, назовите назание тура и количество людей.",
+                          "Позвоните по номеру на сайте, назовите назание тура и количество людей."
                       },
                       "image": {
                         "@type": "ImageObject",
                         "url":
                           "https://trello-attachments.s3.amazonaws.com/5f5623dda5f75f7368d0df19/5f56258edc9f7520ef6036b5/8d2925eccef9e4f78a749b4a9e079d8c/photo5352685267489763103-min.jpg",
                         "height": "406",
-                        "width": "305",
-                      },
-                    },
+                        "width": "305"
+                      }
+                    }
                   ]}
               `,
           }}
